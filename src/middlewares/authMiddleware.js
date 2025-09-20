2b46954e9a8f60ce05d58a110ec3eebb20f15fb9
Acharya Dhruv <acharyadhruv346@gmail.com>
2025-09-20 16:49:07 +0530

    feat(auth): isAdmin middleware with role check

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -16,6 +16,10 @@ const protect = async (req, res, next) => {
 
 
 
-const isAdmin = (req, res, next) => {};
+const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") return res.status(403).json({ error: "Admins only" });
+  next();
+};
+
 
 module.exports = { protect, isAdmin };
