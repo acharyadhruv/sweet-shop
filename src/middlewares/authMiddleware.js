70b1696aa83e29ce00a84008e0c13793929e0ecb
Acharya Dhruv <acharyadhruv346@gmail.com>
2025-09-20 16:44:31 +0530

    feat(auth): protect middleware no-token check

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,4 +1,11 @@
-const protect = (req, res, next) => {};
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/User");
+
+const protect = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "No token provided" });
+};
+
 const isAdmin = (req, res, next) => {};
 
 module.exports = { protect, isAdmin };
