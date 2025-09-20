b3f00971011635fbbcc64596c02ab55baedfc1c4
Acharya Dhruv <acharyadhruv346@gmail.com>
2025-09-20 16:01:39 +0530

    password checked successfully

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -32,14 +32,21 @@ const register = async (req, res) => {
 };
 const login = async (req, res) => {
   const { email, password } = req.body;
-  if (!email || !password) return res.status(500).json({ error: "Email and password are required" });
+
+  if (!email || !password) {
+    return res.status(500).json({ error: "Email and password are required" });
+  }
 
   const user = await User.findOne({ email });
   if (!user) return res.status(401).json({ error: "Invalid credentials" });
 
-  return res.status(200).json({ message: "User found" });
-};
+  // Password check
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(401).json({ error: "Invalid credentials" });
 
+  // Placeholder for next steps
+  return res.status(200).json({ message: "Password matched" });
+};
 
 
 module.exports = { register , login };
